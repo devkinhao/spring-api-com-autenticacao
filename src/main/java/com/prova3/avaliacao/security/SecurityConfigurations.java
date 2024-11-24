6f0e74dfeceec9942cf2a143860a7122fc8fc1a0
Éricke Maas <ericke9maas@gmail.com>
2024-11-24 15:33:51 -0300

    Ajuste feito para liberar acesso ao h2-console

--- src/main/java/com/prova3/avaliacao/security/SecurityConfigurations.java
+++ src/main/java/com/prova3/avaliacao/security/SecurityConfigurations.java
@@ -24,11 +24,13 @@ public class SecurityConfigurations {
 	@Bean
 	public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
 		return httpSecurity.csrf(csrf -> csrf.disable())
+				.headers(headers -> headers.frameOptions(frame -> frame.sameOrigin()))
 				.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
 				.authorizeHttpRequests(authorize -> authorize
 						.requestMatchers(HttpMethod.POST, "/auth/login").permitAll()
 						.requestMatchers(HttpMethod.POST, "/auth/register").permitAll()
 						//.requestMatchers(HttpMethod.POST, "/autores").hasRole("ADMIN")
+						.requestMatchers("/h2-console/**").permitAll() // ajustado para permitir o acesso ao h2-console
 						.anyRequest().authenticated()
 				)
 				.addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class)
